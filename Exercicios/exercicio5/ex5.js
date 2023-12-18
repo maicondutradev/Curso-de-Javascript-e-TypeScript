b533bc3913ec7620e9aadbde8eb1bb29e369d9a7
maicondutradev <109883278+maicondutradev@users.noreply.github.com>
2023-12-18 09:49:00 -0300

    ex5

--- Exercicios/exercicio5/ex5.js
+++ Exercicios/exercicio5/ex5.js
@@ -1,1 +1,10 @@
-const nome = prompt('Digite seu nome: ');
+const nome = prompt('Digite seu nome completo: ');
+document.body.innerHTML += `O seu nome é: ${nome} <br />`;
+document.body.innerHTML += `O seu nome tem ${nome.length} letras <br />`;
+document.body.innerHTML += `A segunda letra do seu nome é: ${nome[1]} <br />`;
+document.body.innerHTML += `Qual o primeiro índice da letra A no seu nome? ${nome.indexOf('a')} <br />`;
+document.body.innerHTML += `Qual o último índice da letra A no seu nome? ${nome.lastIndexOf('a')} <br />`;
+document.body.innerHTML += `As últimas 3 letras do seu nome são: ${nome.slice(-3, nome.length)} <br />`;
+document.body.innerHTML += `As letras do seu nome são: ${nome.split('')} <br />`;
+document.body.innerHTML += `Seu nome com letras maiúsculas: ${nome.toUpperCase()} <br />`;
+document.body.innerHTML += `Seu nome com letras minúsculas: ${nome.toLowerCase()} <br />`;
