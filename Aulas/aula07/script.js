adba8ad29a11aab3c2139afdf8f68a51edcdcda4
maicondutradev <109883278+maicondutradev@users.noreply.github.com>
2023-12-18 16:52:31 -0300

    aula 08

--- Aulas/aula07/script.js
+++ Aulas/aula07/script.js
@@ -1,7 +1,9 @@
-let num1 = 15.2132143523; //Number
+let num1 = 15; //Number
 let num2 = 2.5 //Number
 
 //console.log(num1.toString() + num2);
 //num1 = num1.toString();
 //console.log(num1.toFixed(2));
-
+//console.log(Number.isInteger(num1));
+//let temp = num1 + '5';
+//console.log(Number.isNaN(temp));
---
+++ Aulas/aula08/script.js
@@ -0,0 +1,8 @@
+let num1 = 9.44;
+//let num2 = Math.floor(num1); ARREDONDA O NÚMERO PARA BAIXO
+//let num2 = Math.ceil(num1); ARREDONDA O NÚMERO PARA CIMA
+//let num2 = Math.round(num1); ARREDONDA O NÚMERO AUTOMATICÁMENTE
+//console.log(Math.max(1,2,3,4,5,6,21,43,65,76,21)); ACHAR O MAIOR NÚMERO DA SEQUÊNCIA
+//console.log(Math.min(1,2,3,4,5,6,21,43,65,76,21)); ACHAR O MENOR NÚMERO DA SEQUÊNCIA
+//const aleatorio = Math.round(Math.random() * (10 - 5) + 5);
+//console.log(aleatorio);
