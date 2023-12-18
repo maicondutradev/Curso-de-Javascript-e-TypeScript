39ad385e477353b639c42099c3fdd207d673c21b
maicondutradev <109883278+maicondutradev@users.noreply.github.com>
2023-12-18 08:51:32 -0300

    Aulas e Exercícios

---
+++ Aulas/Aula05/script.js
@@ -0,0 +1,12 @@
+alert('Hello World');
+
+prompt('Digite algo: ');
+
+const comfirma = confirm('Você aceita vender seus dados? ')
+
+if (comfirma == true){
+    alert('Agora seus dados são meus!')
+}else{
+    alert('Por pouco!')
+}
+    
---
+++ Aulas/aula06/script.js
@@ -0,0 +1,83 @@
+//EXPRESSÕES REGULARES
+/*
+               //01234567
+let umaString = "Um texto";
+
+console.log(umaString[4]);
+*/
+
+//----------------------------------------------------
+
+/*
+               //01234567
+let umaString = "Um texto";
+
+console.log(umaString.indexOf('Um', 3));
+console.log(umaString.lastIndexOf('Um', 3));
+*/
+
+//----------------------------------------------------
+
+/*
+               //01234567
+let umaString = "Um texto";
+
+console.log(umString.match(/[a-z]/g));
+*/
+
+//----------------------------------------------------
+
+/*
+               //01234567
+let umaString = "Um texto";
+
+console.log(umaString.search(/[a-z]/));
+*/
+
+//----------------------------------------------------
+
+/*
+               //01234567
+let umaString = "Um texto";
+
+console.log(umaString.replace('Um', 'Outra'));
+console.log(umaString.replace(/U/g, '#'));
+*/
+
+//----------------------------------------------------
+
+/*
+               //01234567
+let umaString = "Um texto";
+
+console.log(umaString.length);
+*/
+
+//----------------------------------------------------
+
+/*
+               //01234567
+let umaString = "Um texto";
+
+console.log(umaString.slice(2,5));
+console.log(umaString.slice(-5, -1));
+*/
+
+//----------------------------------------------------
+
+/*
+               //01234567
+let umaString = "Um texto só hoje parabéns";
+
+console.log(umaString.split(' '));
+*/
+
+//----------------------------------------------------
+
+/*
+               //01234567
+let umaString = "Um texto só hoje parabéns";
+
+console.log(umaString.toUpperCase());
+console.log(umaString.toLowerCase());
+*/
