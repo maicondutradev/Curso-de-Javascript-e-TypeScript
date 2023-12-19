5b646e3d2b6f61298e7d019769632bff20d7977b
maicondutradev <109883278+maicondutradev@users.noreply.github.com>
2023-12-19 09:01:47 -0300

    aula10

---
+++ Aulas Básicas/aula10/funcoes.js
@@ -0,0 +1,45 @@
+/* CRIANDO UMA ARROW FUNCTION SIMPLIFICADA, QUANDO TEM UMA LINHA DE CÓDIGO.
+const raizQuadrada = n => n ** 0.5;
+console.log(raizQuadrada(9));
+*/
+
+
+/* CRIANDO UMA ARROW FUNCTION
+const raizQuadrada = (n) => {
+    return n ** 0.5;
+}
+
+console.log(raizQuadrada(9))
+*/
+
+
+/* CRIANDO UMA FUNÇÃO ANONIMA
+const raizQuadrada = function (n) {
+    return n ** 0.5;
+};
+
+console.log(raizQuadrada(16))
+*/
+
+
+/* CRIANDO UMA FUNÇÃO NORMAL
+function soma (x, y) {
+    const resultado = x * y
+    return resultado
+}
+
+let resultado = soma(15,2)
+
+console.log(resultado);
+*/
+
+
+/* CRIANDO UMA FUNÇÃO NORMAL
+function saudacao(nome){
+    console.log(`Hello World ${nome}!`);
+    return nome;
+}
+
+const variavel =  saudacao('Luiz');
+console.log(variavel);
+*/
