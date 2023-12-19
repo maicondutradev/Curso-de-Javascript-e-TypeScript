4a3a1b8ce0a4defb7db12218cdc6ed397a5de737
maicondutradev <109883278+maicondutradev@users.noreply.github.com>
2023-12-18 22:39:34 -0300

    Aula 09

---
+++ Aulas Básicas/aula09/arrays.js
@@ -0,0 +1,21 @@
+const alunos = ['Maicon', 'Maria', 'Mirela', 'Gabriel'];
+
+console.log(typeof alunos);
+console.log(alunos instanceof Array) //Descobrindo se estou usando uma array
+
+
+//alunos.push('Luiza', 'Erick');
+//console.log(alunos.slice(0,3)); //Recortando a lista do elemento do indice 0 até o indice 3, o 3 não é incluido.
+//console.log(alunos[50]);
+
+//delete alunos[1];
+//console.log(alunos);
+//alunos.shift();
+//alunos.pop(); //Remove o ultimo elemento do array
+//alunos.unshift('Luiza', 'Caio'); //Adicionando um elemento ao início do array
+//alunos.push('Matheus', 'Fábio'); //Adicionando um elemento ao final do array
+//alunos[0] = 'Eduardo'; //Trocando elemento dentro do array
+//alunos[4] = 'Luiza'; //Adicionando uma nova string no fim em um local que não existe
+//console.log(alunos) //Imprimindo a lista inteira
+//console.log(alunos[0]); //Acessando os elementos pelos indices
+//console.log(alunos[2]); //Acessando os elementos pelos indices
